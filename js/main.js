c92133107149f62d9d7cf5ea0db857966a909893
Fabio <fabio.mezzina.09@gmail.com>
2020-11-09 15:43:14 +0100

    markup + style

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    imgUrls: [
+      "./img/image1.jpg",
+      "./img/image2.jpg",
+      "./img/image3.jpg",
+      "./img/image4.jpg"
+    ],
+    imgIndex: 0,
+  },
+});
